c7eda6778c93ebd71e11818811c105777781131f
BYKOfficial <qamin219@gmail.com>
2026-05-07 17:45:00 +0530

    Update API URL to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://byk-market-ai.onrender.com/api'
 });
 
 // Token automatically add karo har request mein
